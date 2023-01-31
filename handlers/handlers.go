5127c7288015d08d5b5fcadc79f201b6d61a20e5
linjiansi <pmon.ken.1467@gmail.com>
2023-02-01 02:15:39 +0900

    introduced go-chi

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -6,52 +6,25 @@ import (
 )
 
 func HelloHandler(w http.ResponseWriter, req *http.Request) {
-	if req.Method == http.MethodGet {
-		io.WriteString(w, "Hello, world! \n")
-	} else {
-		http.Error(w, "Invalid method", http.StatusMethodNotAllowed)
-	}
-
+	io.WriteString(w, "Hello, world! \n")
 }
 
 func PostArticleHandler(w http.ResponseWriter, req *http.Request) {
-	if req.Method == http.MethodPost {
-		io.WriteString(w, "Posting Article... \n")
-	} else {
-		http.Error(w, "Invalid method", http.StatusMethodNotAllowed)
-	}
-
+	io.WriteString(w, "Posting Article... \n")
 }
 
 func GetArticleListHandler(w http.ResponseWriter, req *http.Request) {
-	if req.Method == http.MethodGet {
-		io.WriteString(w, "Article List \n")
-	} else {
-		http.Error(w, "Invalid method", http.StatusMethodNotAllowed)
-	}
-
+	io.WriteString(w, "Article List \n")
 }
 
 func GetArticleDetailHandler(w http.ResponseWriter, req *http.Request) {
-	if req.Method == http.MethodGet {
-		io.WriteString(w, "Article No.1 \n")
-	} else {
-		http.Error(w, "Invalid method", http.StatusMethodNotAllowed)
-	}
+	io.WriteString(w, "Article No.1 \n")
 }
 
 func PostFavoriteArticleHandler(w http.ResponseWriter, req *http.Request) {
-	if req.Method == http.MethodGet {
-		io.WriteString(w, "Posting Nice... \n")
-	} else {
-		http.Error(w, "Invalid method", http.StatusMethodNotAllowed)
-	}
+	io.WriteString(w, "Posting Nice... \n")
 }
 
 func PostCommentHandler(w http.ResponseWriter, req *http.Request) {
-	if req.Method == http.MethodGet {
-		io.WriteString(w, "Posting Comment... \n")
-	} else {
-		http.Error(w, "Invalid method", http.StatusMethodNotAllowed)
-	}
+	io.WriteString(w, "Posting Comment... \n")
 }
--- main.go
+++ main.go
@@ -1,18 +1,25 @@
 package main
 
 import (
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 	"github.com/linjiansi/blog-api/handlers"
 	"log"
 	"net/http"
 )
 
 func main() {
-	http.HandleFunc("/hello", handlers.HelloHandler)
-	http.HandleFunc("/article", handlers.PostArticleHandler)
-	http.HandleFunc("/article/list", handlers.GetArticleListHandler)
-	http.HandleFunc("/article/1", handlers.GetArticleDetailHandler)
-	http.HandleFunc("/article/favorite", handlers.PostFavoriteArticleHandler)
-	http.HandleFunc("/comment", handlers.PostCommentHandler)
+
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+
+	r.Get("/hello", handlers.HelloHandler)
+	r.Post("/article", handlers.PostArticleHandler)
+	r.Get("/article/list", handlers.GetArticleListHandler)
+	r.Get("/article/1", handlers.GetArticleDetailHandler)
+	r.Post("/article/favorite", handlers.PostFavoriteArticleHandler)
+	r.Post("/comment", handlers.PostCommentHandler)
+
 	log.Println("server start at port 8080")
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	log.Fatal(http.ListenAndServe(":8080", r))
 }
