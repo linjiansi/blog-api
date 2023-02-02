895df1cc56976b413c041390d737b34d8fc462aa
linjiansi <pmon.ken.1467@gmail.com>
2023-02-03 02:34:03 +0900

    added query parameter handling to GetArticleHandler

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -5,6 +5,7 @@ import (
 	"github.com/go-chi/chi/v5"
 	"io"
 	"net/http"
+	"strconv"
 )
 
 func HelloHandler(w http.ResponseWriter, req *http.Request) {
@@ -16,7 +17,23 @@ func PostArticleHandler(w http.ResponseWriter, req *http.Request) {
 }
 
 func GetArticleListHandler(w http.ResponseWriter, req *http.Request) {
-	io.WriteString(w, "Article List \n")
+	queryMap := req.URL.Query()
+
+	var page int
+
+	if p, ok := queryMap["page"]; ok && len(p) > 0 {
+		var err error
+		page, err = strconv.Atoi(p[0])
+		if err != nil {
+			http.Error(w, "Invalid query parameter", http.StatusBadRequest)
+			return
+		}
+	} else {
+		page = 1
+	}
+
+	resString := fmt.Sprintf("Article List (page %d)\n", page)
+	io.WriteString(w, resString)
 }
 
 func GetArticleDetailHandler(w http.ResponseWriter, req *http.Request) {
