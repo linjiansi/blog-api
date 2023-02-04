3a375ac0a200be44639c8192c058c8e75aaaab81
linjiansi <pmon.ken.1467@gmail.com>
2023-02-04 13:59:59 +0900

    fixed typo

--- create-table.sql
+++ create-table.sql
@@ -4,13 +4,13 @@ create table if not exists articles (
     contents text not null,
     username varchar(100) not null,
     favorite integer not null,
-    created_at datatime
+    created_at datetime
 );
 
 create table if not exists comments (
     comment_id integer unsigned auto_increment primary key,
     article_id integer unsigned not null,
     message text not null,
-    created_at datatime,
+    created_at datetime,
     foreign key (article_id) references articles(article_id)
 );
