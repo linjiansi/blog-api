071fc83ed71d04e8628a4a6ffbdf9d4b8b9cbc78
linjiansi <pmon.ken.1467@gmail.com>
2023-02-05 01:40:16 +0900

    fixed create table sql

--- create-table.sql
+++ create-table.sql
@@ -4,13 +4,13 @@ create table if not exists articles (
     contents text not null,
     username varchar(100) not null,
     favorite integer not null,
-    created_at datetime
+    created_at datetime not null,
 );
 
 create table if not exists comments (
     comment_id integer unsigned auto_increment primary key,
     article_id integer unsigned not null,
     message text not null,
-    created_at datetime,
+    created_at datetime not null,
     foreign key (article_id) references articles(article_id)
 );
