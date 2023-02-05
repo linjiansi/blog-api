f1ef04fe828b4bd306a8b6f070b3b95cd705fe99
linjiansi <pmon.ken.1467@gmail.com>
2023-02-05 14:10:47 +0900

    fixed setup db sql

--- setupDB.sql
+++ setupDB.sql
@@ -14,3 +14,8 @@ create table if not exists comments (
     created_at datetime not null,
     foreign key (article_id) references articles(article_id)
 );
+
+insert into articles (title, contents, username, favorite, created_at) values ('first article', 'This is my first blog', 'linjiansi', 8, now());
+insert into articles (title, contents, username, favorite, created_at) values ('second article', 'This is my second blog', 'linjiansi', 7, now());
+insert into comments (article_id, message, created_at) values (1, 'first comment', now());
+insert into comments (article_id, message, created_at) values (1, 'second comment', now());
